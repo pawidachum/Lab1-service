eb5e19eb0560cb1c6cc4fb07a5dce2710bfc9619
pawidachum <pawida.may@gmail.com>
2021-03-14 16:17:52 +0700

    finish

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,22 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ProductListComponent } from './product-list/product-list.component';
+import { ProductItemComponent } from './product-item/product-item.component';
+import { PanelComponent } from './panel/panel.component';
+import { BackendService } from './backend.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductListComponent,
+    ProductItemComponent,
+    PanelComponent
   ],
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [BackendService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
